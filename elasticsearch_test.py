60eb44460d8b5e0835148482558d03b2fa7ad7ca
shincling <shijing609@163.com>
2015-11-22 14:50:46 +0800

    first query of the data

--- elasticsearch_test.py
+++ elasticsearch_test.py
@@ -1,16 +1,45 @@
+# -*- coding: utf-8 -*-
 from elasticsearch import Elasticsearch
 import ujson
 import re
-import sys
+import os
 
 es = Elasticsearch('127.0.0.1:9200')
 
-filename='/home/shin/MyGit/Common/MyCommon/430002.json'
-obj = ujson.load(open(filename, 'r'))
-print filename
-if True:
-    es.index('investor', 'investor', obj, id=re.findall(r'\d+',filename))
-    print es
-else:
-    es.index('investor', 'investor', obj)
+path='/home/shin/MyGit/Common/MyCommon/jsonData'
+files=[]
+for file in os.listdir(path):
+    filename=path+'/'+file
+    print filename
+    files.append(filename)
 
+    obj = ujson.load(open(filename, 'r'))
+    if True:
+        es.index('shin_0', 'shin_1', obj, id=re.findall(r'\d+',filename))
+        es.index
+        #print es
+        es.get('shin_0','430002')
+
+
+        pass
+
+
+    else:
+        es.index('shin_0', 'shin_1', obj)
+
+
+res = es.search(
+index='shin_0',
+doc_type='shin_1',
+body={
+    'query': {
+        'match':{
+            'sourceWeb':'touziren'
+        }
+
+
+      }
+    }
+)
+
+print res
