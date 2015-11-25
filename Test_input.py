bf2ca3fc5dfa69dca9bb3352184f39127f8d8014
shincling <shijing609@163.com>
2015-11-25 11:12:03 +0800

    the test of Use the input from CMD ,using sys.argv

--- Test_input.py
+++ Test_input.py
@@ -3,7 +3,15 @@ def hhh(x,y):
     print x+y
 
 #hhh(input('please the first:'),input('please the first:'))
-
+'''
 a1=sys.argv[1]
 a2=sys.argv[2]
 hhh(float(a1),float(a2))
+'''
+
+print "let's start."
+
+cc=raw_input('hi')
+print cc*3
+aa=raw_input('hi2')
+print 'hhh'
