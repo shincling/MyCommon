9f5b7d27194d8d5290458fbcc3c2f1874090f720
shincling <shijing609@163.com>
2015-12-01 01:10:30 +0800

    写入名字选择的函数

--- Test_input.py
+++ Test_input.py
@@ -1,13 +1,14 @@
 #coding:utf8
 import sys
 import re
+
+
 def hhh(x,y):
     print x+y
 
 #hhh(input('please the first:'),input('please the first:'))
 expression=r'''location=北京-北京市&&做市商=xxx&&主券商=XXXX&&crew=<int>'''
 print  re.findall(r'location=([^&|]*)',expression)
-
 
 '''
 a1=sys.argv[1]
@@ -23,3 +24,10 @@ print cc*3
 aa=raw_input('hi2')
 print 'hhh'
 '''
+
+xx=10
+def addone(x):
+    x=x+1
+
+addone(xx)
+pass
