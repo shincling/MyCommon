58777747d1bbc6f2d4bec5da62f8a97361c1f8be
shincling <shijing609@163.com>
2016-01-02 20:23:29 +0800

    增加一些功能和输出的格式化。

--- pdf_relatives/classTheLog.py
+++ pdf_relatives/classTheLog.py
@@ -6,10 +6,12 @@ import os
 
 
 path='/home/shin/Memect/ntdb/data/result/1229/logs_1229'
+path='/home/shin/Memect/ntdb/data/result/0102/logs_0102'
 files=[]
 len_0=0
 len_big=0
 len_count=0
+aa=0
 errorInfinal=0
 for file in os.listdir(path):
     filename=path+'/'+file
@@ -22,9 +24,16 @@ for file in os.listdir(path):
             len_0+=1
         else:
             len_big+=1
+        if int(content[0:-1].split(":")[-1])>10:
+            aa+=1
         len_count+=1
     else:
         errorInfinal+=1
 
-print len_0,len_big,len_count,errorInfinal
+print 'Len=0: %d'%len_0
+print 'Len>0: %d'%len_big
+print 'hasLen: %d'%len_count
+print 'noLen: %d'%errorInfinal
+print 'Len>10:%d'%aa
 
+print float(aa)/len_count
