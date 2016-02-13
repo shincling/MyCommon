cb4b66a21ae912634a5d42f86b6b0a6d24105e61
shincling <shijing609@163.com>
2016-02-13 10:42:47 +0800

    log

--- ML_practice/numpy_practice.py
+++ ML_practice/numpy_practice.py
@@ -1,5 +1,5 @@
 import numpy
-
+'''
 cc=numpy.arange(10)
 bb=cc.reshape(5,2)
 print cc[::-1]
@@ -9,3 +9,9 @@ print bb==1
 
 print numpy.concatenate((bb,bb*2),axis=1)
 print numpy.hstack((bb,bb*3))
+'''
+
+c=numpy.arange(27).reshape(3,3,3)
+print c
+print numpy.vsplit(c,3)
+print c[0,:,0]
