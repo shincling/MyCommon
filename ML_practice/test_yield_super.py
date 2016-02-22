b4f2cb2fb0369b4cbebc830eff4e683614093ff4
shincling <shijing609@163.com>
2016-02-22 17:08:39 +0800

    继续测试，发现外部的变量，会引入到生成器里面？

--- ML_practice/test_yield_super.py
+++ ML_practice/test_yield_super.py
@@ -1,12 +1,16 @@
 def f():
-    msg = yield 'first yield msg'
+    yield 'first yield msg'
     print "generator inner receive:", msg
-    msg = yield 'second yield msg'
-    print "generator inner receive:", msg
+    yield 'second yield msg'
+    print "generator inner2 receive:", msg
+    yield
 
 g = f()
 msg = g.next()
 print "generator outer receive:", msg
-msg = g.send('first send msg')
-print "generator outer receive:", msg
+# msg = g.send('first send msg')
+# print "generator outer receive:", msg
+msg='111'
 g.send('second send msg')
+g.next()
+pass
