52001e2d93899d25f2a7bd05b6bfd6741234a1d2
shincling <shijing609@163.com>
2016-04-02 14:51:54 +0800

    测试begins的使用 一般使用方法是可以在命令行里begin作用的函数 -h 一下就可以看到它的命令行参数如何用

--- test_for_python/func_commandLines.py
+++ test_for_python/func_commandLines.py
@@ -1,12 +1,12 @@
 import sys
 import begin
-@begin.subcommand
+@begin.start
 def ttt(x,y,z='zzz'):
     print x
     print y
     print z
 
-@begin.start
-def run():
-    pass
+# @begin.start
+# def run():
+#     pass
 
