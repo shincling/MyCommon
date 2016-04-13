e5c1f11255f7c10a03b25d056ac671587d5b3565
shincling <shijing609@163.com>
2016-04-13 21:57:12 +0800

    完全懂了，好屌

--- test_for_python/test_argparser.py
+++ test_for_python/test_argparser.py
@@ -6,6 +6,9 @@ args=parser.parse_args()
 print args
 def cc(task,f1=0,f2='ccc'):
     print task,f1,f2
+
+def cc1(f1,f2,task='ccc'):
+    print f1,f2,task
 bb={'task':'hhh'}
 bb1=(11,22,33)
 cc(**args.__dict__)
@@ -15,3 +18,7 @@ cc(**bb)
 cc(bb)
 
 cc(*bb1)
+
+bb2=(22,33)
+
+cc1(*bb2,**bb)
