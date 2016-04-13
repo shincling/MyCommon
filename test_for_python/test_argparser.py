05fd3b30a7e00d5a75a4302c44e494a707bc466d
shincling <shijing609@163.com>
2016-04-13 21:43:44 +0800

    add some feature

--- test_for_python/test_argparser.py
+++ test_for_python/test_argparser.py
@@ -4,9 +4,11 @@ parser.add_argument('--task', type=int, default=222, help='Task#')
 args=parser.parse_args()
 
 print args
-def cc(task):
-    print task
+def cc(task,f1=0,f2='ccc'):
+    print task,f1,f2
 bb={'task':'hhh'}
 cc(**args.__dict__)
+
+cc(**bb)
 
 cc(bb)
