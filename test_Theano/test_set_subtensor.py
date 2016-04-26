5f8b29732142b0eb2835a38581a522c4ba1e17c3
shincling <shijing609@163.com>
2016-04-26 16:41:06 +0800

    三种不同的情况实验

--- test_Theano/test_set_subtensor.py
+++ test_Theano/test_set_subtensor.py
@@ -5,14 +5,22 @@ from theano import tensor as T
 size=10
 x=T.vector('x')
 y=T.matrix('y')
+x1=T.vector('x1')
+x2=T.matrix('all')
+
 
 y=theano.shared(np.random.rand(3,3))
 # y=theano.shared(np.random.randint(3,3),dtype=np.int32)
 
 ff=theano.function([x],updates=[(y,T.set_subtensor(y[0,:],x))])
-# ff1=theano.function([x],updates=[(y,T.set_subtensor(y[(0,0)],x))])
+ff1=theano.function([x1],updates=[(y,T.set_subtensor(y[[1],[1,2]],x1))])
+ff2=theano.function([x2],updates=[(y,T.set_subtensor(y[:,:],x2))])
 input_x=np.array([3.0,3,3])
-input_x1=np.array([0])
-# ff1(input_x1)
+input_x1=np.array([0.,1])
+input_all=np.random.rand(3,3)
+ff1(input_x1)
+print y.get_value()
 ff(input_x)
+print y.get_value()
+ff2(input_all)
 print y.get_value()
