c1943d94ebc09406526ed44dcaeb0b3365f714bc
shincling <shijing609@163.com>
2016-06-02 23:35:20 +0800

    各种设计的改变

--- NLPCC_dbqa/calculate_result.py
+++ NLPCC_dbqa/calculate_result.py
@@ -1,36 +1,33 @@
 import sys
 import re
-def calculate_oneresult(input_path,result_path):
+def construct_input(input_path):
     f_input=open(input_path,'r').readlines()
-    f_result=open(result_path,'r').readlines()
-    assert len(f_input)==len(f_result)
-
     new_question_indexList=[]
     ans_indexList=[]
     old_question=''
     for idx,question in enumerate(f_input):
-        spl=question.split('\t')
+        spl=question.strip().split('\t')
         now_quesiton,now_ans=spl[0],spl[-1]
         if now_quesiton!=old_question:
             new_question_indexList.append(idx)
-        if now_quesiton==1:
+        if now_quesiton=='1':
             ans_indexList.append(idx)
-
+    print len(new_question_indexList),len(ans_indexList)
+    return new_question_indexList,ans_indexList
 
 
 
 
 def main(input_path,result_path):
-    if input_path and result_path:
-        calculate_oneresult(input_path,result_path)
-    else:
+    if not (input_path and result_path):
         print 'Error in path : calculate_result.py %input_path %result_path'
-
-    calculate_oneresult(input_path,)
-
+    print'The baseline result of {}:\n'.format(input_path)
+    quesion_list,ans_list=construct_input(input_path)
 
 
 if __name__=='__main__':
-    input_path=sys.argv[1] if sys.argv[1] else None
-    result_path=sys.argv[2] if sys.argv[2] else None
+    # input_path=sys.argv[1] if sys.argv[1] else '/home/shin/MyGit/Common/MyCommon/NLPCC_dbqa/nlpcc-iccpol-2016.dbqa.training-data'
+    # result_path=sys.argv[2] if sys.argv[2] else None
+    input_path='/home/shin/MyGit/Common/MyCommon/NLPCC_dbqa/nlpcc-iccpol-2016.dbqa.training-data'
+    result_path=None
     main(input_path,result_path)
