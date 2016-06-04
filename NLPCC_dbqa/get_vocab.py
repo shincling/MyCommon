c23be1241f3c4e371ae6984b948b8bf07428e36c
shincling <shijing609@163.com>
2016-06-04 22:17:08 +0800

    finish the get_vocab and pickle them into VocabSet_in_NLPCC

--- NLPCC_dbqa/get_vocab.py
+++ NLPCC_dbqa/get_vocab.py
@@ -1,6 +1,7 @@
 #coding=utf8
 import sys
 import re
+import pickle
 import jieba
 
 input_path='/home/shin/MyGit/Common/MyCommon/NLPCC_dbqa/nlpcc-iccpol-2016.dbqa.training-data'
@@ -9,19 +10,22 @@ print 'total lines of input is {}'.format(len(f_input))
 sent_set=set()
 question_set=set()
 ans_set=set()
+vocab=set()
 for line in f_input:
     each=line.split('\t')
-    question_set.add(each[0])
+    # question_set.add(each[0])
+    # ans_set.add(each[1])
+    # if each[1] in ans_set:
+    #     print each[1]
     sent_set.add(each[0])
     sent_set.add(each[1])
-    if each[1] in ans_set:
-        print each[1]
-    ans_set.add(each[1])
 print 'total num of sents:{}'.format(len(sent_set))
-print len(question_set)
-print len(ans_set)
+# print len(question_set)
+# print len(ans_set)
+for sent in sent_set:
+    words=jieba._lcut(sent)
+    for word in words:
+        vocab.add(word)
+print len(vocab)
+pickle.dump(vocab,open('vocabSet_in_NLPCC','w'))
 
-for ans in ans_set:
-    if ans in question_set:
-        print ans
-        break
