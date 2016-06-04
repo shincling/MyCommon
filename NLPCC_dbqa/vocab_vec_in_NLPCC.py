628e586ec0185d4bdcd36e8be4f329f6ee98e809
shincling <shijing609@163.com>
2016-06-05 01:06:08 +0800

    fix the bug that vocabSet is Unicode to UTF8

--- NLPCC_dbqa/vocab_vec_in_NLPCC.py
+++ NLPCC_dbqa/vocab_vec_in_NLPCC.py
@@ -4,6 +4,12 @@ import numpy as np
 import pickle
 
 vocab_NLPCC=pickle.load(open('vocabSet_in_NLPCC'))
-vocab_total=pickle.load(open('word2vec_wordlit_20160603'))
+vocab_total=pickle.load(open('word2vec_wordlist_20160603'))
 print 'vocab_nlpcc:{}'.format(len(vocab_NLPCC))
 print 'vocab_total:{}'.format(len(vocab_total))
+
+cover_idx=0
+for word in vocab_NLPCC:
+    if word.encode('utf8') in vocab_total:
+       cover_idx+=1
+print float(cover_idx)/len(vocab_NLPCC)
