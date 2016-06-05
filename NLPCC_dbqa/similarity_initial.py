8c2d7e4156d1decfdb887b3277d351e1ab135542
shincling <shijing609@163.com>
2016-06-05 13:47:45 +0800

    fix some bugs

--- NLPCC_dbqa/similarity_initial.py
+++ NLPCC_dbqa/similarity_initial.py
@@ -7,23 +7,25 @@ import jieba
 def cos_dis(vector1,vector2):
     return dot(vector1,vector2)/(linalg.norm(vector1)*linalg.norm(vector2))
 
-word_dict=pickle.load('nlpcc_dict_20160605')
+word_dict=pickle.load(open('nlpcc_dict_20160605'))
 input_path='/home/shin/MyGit/Common/MyCommon/NLPCC_dbqa/nlpcc-iccpol-2016.dbqa.training-data'
 f_input=open(input_path,'r').readlines()
 print 'total lines of input is {}'.format(len(f_input))
 dis_list=[]
 for line in f_input:
     each=line.split('\t')
-    question=jieba._lcut(each[1])
+    question=jieba._lcut(each[0])
     question_vector=zeros(100)
+    # print question_vector.shape
     for word in question:
-        one_vec=word_dict[word]
+        one_vec=word_dict[word.encode('utf8')]
+        # print one_vec.shape
         question_vector+=one_vec
 
     answer=jieba._lcut(each[1])
     answer_vector=zeros(100)
     for word in answer:
-        one_vec=word_dict[word]
+        one_vec=word_dict[word.encode('utf8')]
         answer_vector+=one_vec
 
     dis=cos_dis(question_vector,answer_vector)
