1c9728969c4056d507f9f2e547d696a20f410b4a
shincling <shijing609@163.com>
2016-06-08 17:13:22 +0800

    shuffle~shuffle~shuffle

--- NLPCC_dbqa/data_valid/split_valid.py
+++ NLPCC_dbqa/data_valid/split_valid.py
@@ -1,7 +1,11 @@
 #coding=utf8
 import sys
 sys.path.append('..')
+import random
 import calculate_result
+
+ratio=0.3
+shuffle_times=10
 
 input_path='/home/shin/MyGit/Common/MyCommon/NLPCC_dbqa/nlpcc-iccpol-2016.dbqa.training-data'
 result_path=None
@@ -16,4 +20,28 @@ for idx,ques in enumerate(full_list):
     except IndexError:
         break
     question_rangelist.append(range)
+
+content_list=[]
+for range in question_rangelist:
+    content_list.append(f_input[range[0]:range[1]])
+print 'total content covers {} stories.'.format(len(content_list))
+
+# for _ in range(10):
+#     random.shuffle(content_list)
+#     print 'Shuffle ~~~~~  ! '
+#
+random.shuffle(content_list)
+print 'Shuffle ~~~~~  ! '
+random.shuffle(content_list)
+print 'Shuffle ~~~~~  ! '
+random.shuffle(content_list)
+print 'Shuffle ~~~~~  ! '
+random.shuffle(content_list)
+print 'Shuffle ~~~~~  ! '
+random.shuffle(content_list)
+print 'Shuffle ~~~~~  ! '
+
+part_index=int(len(content_list)*(1-ratio))
+print 'The train coves {} stories,and the valid covers {} stories'.format(part_index,len(content_list)-part_index)
+
 
