262f70a0e0c7af54845c7e7d99208794a6b7f456
shincling <shijing609@163.com>
2016-06-08 17:59:21 +0800

    turning some params

--- NLPCC_dbqa/examples_in_official.py
+++ NLPCC_dbqa/examples_in_official.py
@@ -3,8 +3,8 @@ import xgboost as xgb
 dtrain = xgb.DMatrix('similarity_withLap_0607')
 dtest = xgb.DMatrix('similarity_withLap_0607')
 # specify parameters via map
-param = {'max_depth':2, 'eta':1, 'silent':1, 'objective':'binary:logistic' }
-num_round = 2
+param = {'max_depth':50, 'eta':1, 'silent':0, 'objective':'binary:logistic' }
+num_round = 80
 bst = xgb.train(param, dtrain, num_round)
 # make prediction
 preds = bst.predict(dtest)
