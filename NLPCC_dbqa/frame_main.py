f10b8b9ff1cb236b29366c090e61f79a659b4523
shincling <shijing609@163.com>
2016-06-14 10:52:05 +0800

    build the construct function to split the data

--- NLPCC_dbqa/frame_main.py
+++ NLPCC_dbqa/frame_main.py
@@ -26,7 +26,7 @@ def get_vocab(train_file,test_file):
     print 'Save the set to {}'.format(output_name)
     return vocab
 
-def construct_input(input_path):
+def construct_toral_input(input_path):
     f_input_train=open(train_file,'r').readlines()
     f_input_test=open(test_file,'r').readlines()
     f_input=f_input_train+f_input_test
@@ -48,6 +48,44 @@ def construct_input(input_path):
     print 'total num of ans=1 list is {}'.format(len(ans_indexList))
     return new_question_indexList,ans_indexList,f_input
 
+def construct_train(input_path):
+    f_input=open(input_path,'r').readlines()
+    print 'total lines of train file is {}'.format(len(f_input))
+    new_question_indexList=[]
+    ans_indexList=[]
+    result_list=[]
+    old_question=''
+    for idx,question in enumerate(f_input):
+        spl=question.strip().split('\t')
+        now_quesiton,now_ans=spl[0],spl[2]
+        if now_quesiton!=old_question:
+            new_question_indexList.append(idx)
+            old_question=now_quesiton
+        if now_ans=='1':
+            ans_indexList.append(idx)
+        result_list.append(spl[-1])
+    print 'total num of questions is :{}\n'.format(len(new_question_indexList))
+    return new_question_indexList,ans_indexList,f_input
+
+def construct_test(input_path):
+    f_input=open(input_path,'r').readlines()
+    print 'total lines of test file is {}'.format(len(f_input))
+    new_question_indexList=[]
+    ans_indexList=[]
+    result_list=[]
+    old_question=''
+    for idx,question in enumerate(f_input):
+        spl=question.strip().split('\t')
+        now_quesiton,now_ans=spl[0],spl[2]
+        if now_quesiton!=old_question:
+            new_question_indexList.append(idx)
+            old_question=now_quesiton
+        if now_ans=='1':
+            ans_indexList.append(idx)
+        result_list.append(spl[-1])
+    print 'total num of questions is :{}\n'.format(len(new_question_indexList))
+    return new_question_indexList,ans_indexList,f_input
+
 if __name__=='__main__':
     train_file='/home/shin/MyGit/Common/MyCommon/NLPCC_dbqa/data_valid/train7_1'
     test_file='/home/shin/MyGit/Common/MyCommon/NLPCC_dbqa/data_valid/valid3_1'
@@ -56,3 +94,5 @@ if __name__=='__main__':
         vocab=get_vocab(train_file,test_file)
     else:
         vocab=pickle.load(open('vocabSet_in_NLPCC_main'))
+    construct_train(train_file)
+    construct_test(test_file)
