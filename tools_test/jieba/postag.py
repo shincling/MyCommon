3bc7f97ff97012ef4f2681c8342433ed02ffefd9
shincling <shijing609@163.com>
2016-06-24 15:29:35 +0800

    add some patterns

--- tools_test/jieba/postag.py
+++ tools_test/jieba/postag.py
@@ -2,5 +2,7 @@
 import jieba.posseg as pseg
 words =pseg.cut("iphone6分辨率是多少？")
 words =pseg.cut("是谁把你带到孙小虎身边。")
+words =pseg.cut("青海高原牦牛毛色有什么特点？")
+words =pseg.cut("请问霍建华喜欢哪3只小狗  ")
 for w in words:
    print w.word,w.flag
