4c986c796345264596704c23256dff583abd9faa
shincling <shijing609@163.com>
2016-07-02 12:56:42 +0800

    show all lines

--- NLPCC_dbqa/data_valid/show_error.py
+++ NLPCC_dbqa/data_valid/show_error.py
@@ -6,7 +6,7 @@ def main(train_file,test_file,train_result,test_result):
     test_result=open(test_result).readlines()
     assert len(test_file)==len(test_result)
 
-    output_file=open('error_count_1.txt','a')
+    output_file=open('error_count_all.txt','a')
     for line,score in zip(test_file,test_result):
         spl=line.split('\t')
         question=spl[0]
@@ -14,7 +14,10 @@ def main(train_file,test_file,train_result,test_result):
         target=float(spl[-1].strip())
         score=float(score)
         if abs(target-score)>0.5:
+            output_file.write(line.strip()+'\t'+str(score)+'Error!'+'\n')
+        else:
             output_file.write(line.strip()+'\t'+str(score)+'\n')
+
 
 
 
@@ -24,6 +27,6 @@ if __name__=='__main__':
     train_file='train7_1'
     test_file='valid3_1'
     train_result='/home/shin/MyGit/Common/MyCommon/NLPCC_dbqa/results/result_0629_allmix_train'
-    test_result='/home/shin/MyGit/Common/MyCommon/NLPCC_dbqa/results/result_0629_allmix_valid'
+    test_result='/home/shin/MyGit/Common/MyCommon/NLPCC_dbqa/results/result_0630_allmix_1_valid'
     main(train_file,test_file,train_result,test_result)
 
