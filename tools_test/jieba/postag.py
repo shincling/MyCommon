9fcac550ea9e8c54df6108a837391cabada06583
shincling <shijing609@163.com>
2016-07-21 11:16:22 +0800

    find some examples

--- tools_test/jieba/postag.py
+++ tools_test/jieba/postag.py
@@ -6,6 +6,7 @@ words =pseg.cut("青海高原牦牛毛色有什么特点？")
 words =pseg.cut("'潘玉利什么时候担任过任江苏省宿迁市人大常委会党组成员、秘书长、办公室主任？'")
 words =pseg.cut("邓小平的妻子是谁？	")
 words =pseg.cut("金门县议会的议员任期是多久？")
+words =pseg.cut("布什什么时候来过南京赛珍珠故居？")
 for w in words:
    print w.word,w.flag
 
