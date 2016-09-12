df9d8b7bcfbd2c567cba4cdd4864553d2d058342
shincling <shijing609@163.com>
2016-09-12 20:59:15 +0800

    传递形参给函数 但是有问题 无法实现返回之类的功能’
    ’

--- linux_bash/function_test.sh
+++ linux_bash/function_test.sh
@@ -11,3 +11,15 @@ echo Hi,world
 
 hello
 hi
+
+function plus()
+{
+#echo 'expr $1 + $2'
+E= expr $1 + $2
+}
+#D= 'plus 100 200'
+#echo 'D='$D
+
+# 这里传递参数有各种问题 之后要专门找个地方好好看一看，加引号完全好像没办法work的
+plus 100 300
+echo 'E='$E
