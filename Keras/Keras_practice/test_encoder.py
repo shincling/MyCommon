049f82c976739d743ce1fe6917b637013766c624
honeydeer <shiwei2013@ia.ac.cn>
2016-09-13 14:16:16 +0800

    try to convert to float32 , but not work.

--- Keras/Keras_practice/test_encoder.py
+++ Keras/Keras_practice/test_encoder.py
@@ -1,5 +1,5 @@
 __author__ = 'shin'
-
+import fpectl
 from keras.models import Sequential
 from keras.layers import Merge, LSTM, Dense
 import numpy as np
@@ -29,14 +29,19 @@ decoder.compile(loss='categorical_crossentropy',
 x_train_a = np.random.random((1000, timesteps, data_dim))
 x_train_b = np.random.random((1000, timesteps, data_dim))
 y_train = np.random.random((1000, nb_classes))
-
+x_train_a=np.float32(x_train_a)
+x_train_b=np.float32(x_train_b)
+y_train=np.float32(y_train)
 # generate dummy validation data
 x_val_a = np.random.random((100, timesteps, data_dim))
 x_val_b = np.random.random((100, timesteps, data_dim))
 y_val = np.random.random((100, nb_classes))
-
+x_val_a=np.float32(x_train_a)
+x_val_b=np.float32(x_train_b)
+y_val=np.float32(y_train)
+# fpectl.turnon_sigfpe()
 decoder.fit([x_train_a, x_train_b], y_train,
-            batch_size=64, nb_epoch=15,
+            batch_size=10000, nb_epoch=15,
             validation_data=([x_val_a, x_val_b], y_val))
 # decoder.fit([x_train_a, x_train_b], y_train,
 #             batch_size=64, nb_epoch=15)
