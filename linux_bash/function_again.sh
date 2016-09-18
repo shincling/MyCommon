23c111bb73e96f03edabc59202b6da3890d82465
shincling <shijing609@163.com>
2016-09-18 15:39:23 +0800

    sum 还是有问题，之后计数的程序可以参考

---
+++ linux_bash/function_again.sh
@@ -0,0 +1,24 @@
+# !/bin/bash/
+sum()
+{
+　a = $1;
+　b = $2;
+　echo $[$a + $b ];
+}
+sum 3 4
+
+function LoopPrint()
+{
+    count=0;
+    while [ $count -lt $1 ];
+    do
+    echo $count;
+    let ++count;
+    sleep 1;
+    done
+    return 0;
+}
+
+read -p "Please input the times of print you want: " n;
+LoopPrint $n;
+
