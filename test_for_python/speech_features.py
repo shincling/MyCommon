3dba46e77e99042cc59044950c10af0d4287e2d4
shincling <shijing609@163.com>
2017-08-01 15:24:57 +0800

    add some explations

--- test_for_python/speech_features.py
+++ test_for_python/speech_features.py
@@ -1,3 +1,4 @@
+#coding=utf8
 from python_speech_features import fbank
 from python_speech_features import logfbank
 import scipy.io.wavfile as wav
@@ -5,7 +6,7 @@ import scipy.io.wavfile as wav
 path='/home/sw/Shin/Codes/DL4SS_Keras/Data_with_dev/male_test.wav'
 (rate,sig)=wav.read(path)
 print (rate,sig)
-print sig.shape
-fbank_feat=fbank(sig,rate,nfilt=40)
-print fbank_feat[0].shape
+print sig.shape #43520
+fbank_feat=fbank(sig,rate,winstep=0.01,nfilt=40)
+print fbank_feat[0].shape  # 271的结果是这样得到噢的，43520/(0.01s*16000)
 
