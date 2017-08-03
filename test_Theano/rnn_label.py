fe19638e2594ed0c9f9c870cb57c6d44de7fb536
shincling <shijing609@163.com>
2017-08-03 16:59:26 +0800

    调了下参数，这个看样子还不错，初始化不太稳定，多跑几次不出现cost　nan就能继续跑。

--- test_Theano/rnn_label.py
+++ test_Theano/rnn_label.py
@@ -13,22 +13,23 @@ output_size=1
 batch_size=32
 total_epoch=100
 length=10
-num_train=10000
+num_train=50000
 num_test=1000
 
 X=T.tensor3()
 Y=T.imatrix()
 # Y=T.iscalar()
 l_in=lasagne.layers.InputLayer(shape=(None,None,hidden_size))
-rnn=lasagne.layers.GRULayer(l_in,output_size,only_return_final=True)
+rnn=lasagne.layers.GRULayer(l_in,output_size,hid_init=lasagne.init.Normal(std=1),only_return_final=True)
 output=lasagne.layers.flatten(rnn)
+output=rnn
 # output=lasagne.layers.DenseLayer(rnn,1,nonlinearity=lasagne.nonlinearities.sigmoid)
 
 result=lasagne.layers.helper.get_output(output,{l_in:X})
 cost=T.nnet.binary_crossentropy(result,Y).sum()
 params = lasagne.layers.helper.get_all_params(output, trainable=True)
 grads = T.grad(cost, params)
-updates = lasagne.updates.rmsprop(grads, params, learning_rate=0.0005)
+updates = lasagne.updates.rmsprop(grads, params, learning_rate=0.00001)
 
 train_model = theano.function([X,Y], [cost,result], updates=updates)
 
@@ -36,15 +37,15 @@ train_model = theano.function([X,Y], [cost,result], updates=updates)
 num_batch_train=num_train/batch_size
 num_batch_test=num_test/batch_size
 train_data=create_sequence(num_train,length,1)
-train_data[:int(0.1*num_train)]=np.zeros([length,1])
+train_data[:int(0.3*num_train)]=np.zeros([length,1])
 for _ in range(total_epoch):
     np.random.shuffle(train_data)
     for idx_batch in range(num_batch_train):
         x=train_data[idx_batch*batch_size:(idx_batch+1)*batch_size]
-        y_label=np.int32(np.count_nonzero(x,axis=1)==0)
+        y_label=1-np.int32(np.count_nonzero(x,axis=1)==0)
         cost,result=train_model(x,y_label)
-        print 'cost:{},result:{}'.format(cost,result.flatten())
-        print 'y_label:',y_label.flatten()
+        print 'cost:{},result:{}'.format(cost,result.flatten()[:10])
+        print 'y_label:',y_label.flatten()[:10]
 
 
 
