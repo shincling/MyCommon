2596a004ffb9f27aedba286981c0b027a7285d68
shincling <shijing609@163.com>
2017-09-18 21:47:02 +0800

    测试了下 future里面的print

--- test_for_python/iter_else.py
+++ test_for_python/iter_else.py
@@ -1,7 +1,12 @@
+from __future__ import print_function
 i=7
 while i>0:
     if i%7==0:
         break
     i-=1
 else:
-    print 'hhh'
+    print('hhh')
+
+
+for i in range(10):
+    print(i,end=' ')
