3e8421d539e3942891c537d538751b15f2947128
shincling <shijing609@163.com>
2017-11-09 22:05:18 +0800

    test and work through the V3 model with the pre_trained path saveed in this root.

---
+++ Pytorch/CNN_test/load_model.py
@@ -0,0 +1,24 @@
+import torch
+from torch import nn
+from torch.autograd import Variable
+import torch.nn.functional as F
+import torch.optim as optim
+import torchvision.models as models
+import myNet
+
+torch.manual_seed(1)
+
+# class VIDEO_QUERY(nn.Module):
+#     def __init__(self,total_frames,video_size):
+#         self.images_net=models.inception_v3(pretrained=True)
+#
+#     def forward(self,x):
+
+# mm=models.inception_v3()
+mm=myNet.inception_v3(1)
+#
+xx=Variable(torch.rand([2,3,299,299]))
+print mm(xx)
+
+
+
