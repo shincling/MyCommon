69faf9d1b94ed14f10430c25c7991b5100192fc8
shicnling <shijing609@163.com>
2018-09-04 11:53:16 +0800

    验证了一下，这个插值函数应该是可靠的。

--- Pytorch/test_autograd_interpolation.py
+++ Pytorch/test_autograd_interpolation.py
@@ -4,7 +4,19 @@ from torch.autograd import Variable
 import numpy as np
 from torchvision.transforms import *
 
+def interpolate(var,size,axis=1):
+    # 要求输入是二维图像，插值的是后面的这一个维度！
+    # cc=Variable(torch.random(6*256,75),requires_grad=True)
+    # var=cc
+    shape=var.size()
+    assert len(shape)==2
+    out_var=Variable(torch.zeros(shape[0],size),requires_grad=0).cuda()
+    for i in range(size):
+        out_var[:,i]=var[:,i*shape[1]/size]
+    return out_var.view(shape[0],size)
+
 toPIL=ToPILImage()
+
 def mul3(x):
     return x*3
 def add3(x):
@@ -13,9 +25,15 @@ def add3(x):
     # return x+torch.FloatTensor(np.array([[1,2],[10,11]]))
 def mul3_numpy(x):
     return Variable(torch.FloatTensor(x.data.numpy()*3))
-a=Variable(torch.ones(2,2),requires_grad=True)
+a=Variable(torch.rand(2,2),requires_grad=True)
 
 b=a*2
+tt=interpolate(a,3)
+print tt
+tt.sum().backward()
+print a.grad
+print b.grad
+1/0
 cc=toPIL(b)
 
 c=b*b
